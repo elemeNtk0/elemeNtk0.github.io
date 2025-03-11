024d0f0608c40c3e049763717f8539fe17be17c9
elemeNtk0 <elemeNtk0gm@gmail.com>
2025-03-11 10:27:55 +0300

    trash

--- energy-research/description.php
+++
@@ -1,8 +0,0 @@
-<?
-$arTemplate = array (
-  'NAME' => 'Шаблон ЦЭИ',
-  'DESCRIPTION' => '',
-  'SORT' => '',
-  'TYPE' => '',
-);
-?>
--- energy-research/footer.php
+++
@@ -1,8 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-
-
-<script src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH."/js/jquery-1.11.1.min.js")?>"></script>
-
-
-
-</body></html>
